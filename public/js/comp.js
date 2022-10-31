68ded0cbd6e50a0c0062f1ed4f5ac2a81147c857
Nikita Gurskiy <85452618+lilipops@users.noreply.github.com>
2022-10-31 11:43:35 +0100

    Add files via upload

---
+++ public/js/comp.js
@@ -0,0 +1,19 @@
+import HeaderComp from './components/headercomponent';
+import CartComp from './components/cart-comp';
+import HeroComp from './components/herocomponent';
+import PromoComp from './components/promocomponent';
+import ProductGridComp from './components/productgridcomponent';
+import BenefitsComp from './components/benefitscomponent';
+import FooterComp from './components/footercomponent';
+import Error from './components/error-comp';
+
+export default {
+    components: HeaderComp,
+    CartComp,
+    HeroComp,
+    PromoComp,
+    ProductGridComp,
+    BenefitsComp,
+    FooterComp,
+    Error
+}
